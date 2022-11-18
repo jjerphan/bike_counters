627e2cf18e8c56905a1d5c21ae2c3d222f7a3966
Julien Jerphanion <git@jjerphan.xyz>
2022-11-18 14:52:19 +0100

    Extend keras.Sequential to be able to inject parameters via closure

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -1,11 +1,17 @@
-import pandas as pd
+import keras
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import Ridge
 
+BATCH_SIZE = 32
+EPOCHS = 10
+
+class ExtendedSequential(keras.Sequential):
+    def fit(self, X, y):
+
+        # TODO: Update the logic here
+        return super().fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS)
 
 def _encode_dates(X):
     X = X.copy()  # modify a copy of X
@@ -21,7 +27,7 @@ def _encode_dates(X):
 
 
 def get_estimator():
-    date_encoder = FunctionTransformer(_encode_dates)
+    date_encoder = OneHotEncoder(handle_unknown="ignore")
     date_cols = ["year", "month", "day", "weekday", "hour"]
 
     categorical_encoder = OneHotEncoder(handle_unknown="ignore")
@@ -29,12 +35,23 @@ def get_estimator():
 
     preprocessor = ColumnTransformer(
         [
-            ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+            ("date", date_encoder, date_cols),
             ("cat", categorical_encoder, categorical_cols),
         ]
     )
-    regressor = Ridge()
 
-    pipe = make_pipeline(date_encoder, preprocessor, regressor)
+    date_encoder = FunctionTransformer(_encode_dates)
+
+    # TODO: Update the logic here
+    keras_regressor = ExtendedSequential()
+    keras_regressor.add(keras.layers.Dense(8))
+    keras_regressor.add(keras.layers.Dense(1))
+    keras_regressor.compile(optimizer='sgd', loss='mse')
+
+    pipe = make_pipeline(
+        date_encoder,
+        preprocessor,
+        keras_regressor,
+    )
 
     return pipe
